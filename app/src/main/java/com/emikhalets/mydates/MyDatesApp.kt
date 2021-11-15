5e8b9cc3e24c1ab06d98adbe580b12aba1182882
Mikhalets Evgeniy <evg.mikhalets@gmail.com>
2021-11-15 23:49:09 +0300

    refactoring switching themes

--- app/src/main/java/com/emikhalets/mydates/MyDatesApp.kt
+++ app/src/main/java/com/emikhalets/mydates/MyDatesApp.kt
@@ -1,13 +1,11 @@
 package com.emikhalets.mydates
 
 import android.app.Application
-import androidx.appcompat.app.AppCompatDelegate
 import com.emikhalets.mydates.utils.AppAlarmManager
 import com.emikhalets.mydates.utils.di.AppComponent
 import com.emikhalets.mydates.utils.di.DaggerAppComponent
 import com.emikhalets.mydates.utils.DEFAULT_NOTIFICATION_HOUR
 import com.emikhalets.mydates.utils.DEFAULT_NOTIFICATION_MINUTE
-import com.emikhalets.mydates.utils.enums.Theme
 import com.emikhalets.mydates.utils.extentions.launchMainScope
 
 class MyDatesApp : Application() {
@@ -17,7 +15,6 @@ class MyDatesApp : Application() {
     override fun onCreate() {
         super.onCreate()
         buildDaggerDependencies()
-        checkTheme()
         initNotifications()
     }
 
@@ -33,15 +30,6 @@ class MyDatesApp : Application() {
                 appComponent.appPreferences.setNotificationHour(DEFAULT_NOTIFICATION_HOUR)
                 appComponent.appPreferences.setNotificationMinute(DEFAULT_NOTIFICATION_MINUTE)
                 AppAlarmManager.scheduleAllAlarms(applicationContext)
-            }
-        }
-    }
-
-    private fun checkTheme() {
-        launchMainScope {
-            when (Theme.get(appComponent.appPreferences.getTheme())) {
-                Theme.LIGHT -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                Theme.DARK -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
             }
         }
     }
